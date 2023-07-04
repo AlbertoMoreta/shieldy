6be6fb33be013e7b73493780d917fac65aff8857
Alberto Moreta <albertomoreta70@gmail.com>
2023-07-04 13:21:05 +0200

    Docker and mongo changes

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+_js_escape() {
+  printf '%s\n' "$1" | sed -e 's/[]\/$*.^[]/\\&/g'
+}
+
 if [ ! -z ${MONGO_SHIELDY_PASSWORD_FILE+x} ]; then
   MONGO_SHIELDY_PASSWORD=$(cat ${MONGO_SHIELDY_PASSWORD_FILE})
 fi
 
+
+if [ ! -z ${MONGO_INITDB_ROOT_PASSWORD_FILE+x} ]; then
+  MONGO_INITDB_ROOT_PASSWORD=$(cat ${MONGO_INITDB_ROOT_PASSWORD_FILE})
+fi
+
+
 if [ "$MONGO_SHIELDY_USERNAME" ] && [ "$MONGO_SHIELDY_PASSWORD" ]; then
-  "${mongo[@]}" -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "$rootAuthDatabase" "$MONGO_INITDB_DATABASE" <<-EOJS
+  mongosh mongodb://localhost:27017/admin -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" <<-EOJS
+    use $MONGO_INITDB_DATABASE;
     db.createUser({ 
-      user: $(_js_escape "$MONGO_SHIELDY_USERNAME"), 
-      pwd: $(_js_escape "$MONGO_SHIELDY_PASSWORD"), 
-      roles: [{ role: "readWrite", db: $(_js_escape "$MONGO_INITDB_DATABASE")}]
+      user: "$MONGO_SHIELDY_USERNAME", pwd: "$MONGO_SHIELDY_PASSWORD", roles: [{ role: "readWrite", db: "$MONGO_INITDB_DATABASE"}]
     });
 EOJS
 fi
